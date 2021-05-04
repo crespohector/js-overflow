eb30dfdb51bfb47298b77b65697805ab5a277673
hector <crespomhector@gmail.com>
2021-05-04 16:01:21 -0400

    commit two

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
