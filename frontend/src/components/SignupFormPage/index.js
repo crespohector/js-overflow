38b67128a8f9bc806439bcccf3e90a9aa0f9f1bd
Hector <crespomhector@gmail.com>
2021-07-04 21:57:05 -0400

    refactor the sign up page

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -1,8 +1,11 @@
 // frontend/src/components/SignupFormPage/index.js
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Redirect, NavLink } from "react-router-dom";
 import * as sessionActions from "../../store/session";
+
+import Footer from '../Footer/';
+import NavBar from '../NavBar/';
 import './SignupForm.css';
 
 
@@ -31,56 +34,77 @@ function SignupFormPage() {
   };
 
   return (
-    <form className="signup_form" onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>
-      <div>
-        <label>
-          Email
-        <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
+    <>
+      <NavBar />
+      <form className="signup_form" onSubmit={handleSubmit}>
+        <ul>
+          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+        </ul>
+        <div>
+          <label className="login_form-label_email">
+            Email
+            <div>
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="login_form-input_email"
+                required
+              />
+            </div>
+          </label>
+        </div>
+        <div>
+          <label className="login_form-label_email">
+            Username
+            <div>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="login_form-input_email"
+                required
+              />
+            </div>
+          </label>
+        </div>
+        <div>
+          <label className="login_form-label_password">
+            Password
+            <div>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="login_form-input_password"
+                required
+              />
+            </div>
+          </label>
+        </div>
+        <div>
+          <label className="login_form-label_password">
+            Confirm Password
+            <div>
+              <input
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className="login_form-input_password"
+                required
+              />
+            </div>
+          </label>
+        </div>
+        <button className="login_form-btn_submit" type="submit">Sign Up</button>
+      </form>
+      <div className="sign_up_link_container">
+        <span className="sign_up_span" >Already have an account? <NavLink className="sign_up_link" to="/login">Log in</NavLink></span>
       </div>
-      <div>
-        <label>
-          Username
-        <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </label>
+      <div className="login_footer">
+        <Footer />
       </div>
-      <div>
-        <label>
-          Password
-        <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          Confirm Password
-        <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </label>
-      </div>
-      <button type="submit">Sign Up</button>
-    </form>
+    </>
   );
 }
 
