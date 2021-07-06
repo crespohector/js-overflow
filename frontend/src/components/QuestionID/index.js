e84bc96d268d9db3224b26ac662c8dbc16f8e073
Hector <crespomhector@gmail.com>
2021-07-06 17:12:27 -0400

    added a count variable to keep track of how many answers are in a question

--- frontend/src/components/QuestionID/index.js
+++ frontend/src/components/QuestionID/index.js
@@ -1,13 +1,10 @@
-/* IMPORTANT: when the user reloads the page, it restarts the redux store
-  ---Notice: I noticed how when we have two dispatches (which is synchronous), it will render the answers first
-            then render the question. You will notice a delay when refreshing the page which may look unpleasant. Will fix*/
-
 import React, { useEffect, useState } from 'react';
-import { NavLink, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Modal from "react-modal"
 import { useSelector, useDispatch } from 'react-redux';
-import {  getAnswers, addOneAnswer } from '../../store/answers';
+import { getAnswers, addOneAnswer } from '../../store/answers';
 import { getQuestions } from '../../store/questions';
+import NavBar from '../NavBar';
 import './QuestionID.css';
 
 Modal.setAppElement('#root');
@@ -23,6 +20,7 @@ function QuestionID() {
   const questions = useSelector(state => state.questions);
   const question = Object.values(questions).find(question => question.id == questionId);
   const user = useSelector(state => state.session.user);
+  const countAnswers = answersArr.length;
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -40,7 +38,6 @@ function QuestionID() {
 
   useEffect(() => {
     const errors = [];
-
     if (comment.length < 30) {
       errors.push("Comment field must be greater than 30 characters");
     }
@@ -50,41 +47,44 @@ function QuestionID() {
     setErrors(errors);
   }, [comment])
 
+
   return (
-    <div className="QuestionID-main-container">
-      <div className="QuestionID-head-container">
-        <span className="QuestionID-text">All Answers</span>
-        <NavLink className="QuestionID-home" to="/">Home</NavLink>
+    <>
+      <NavBar />
+      <div className="QuestionID-main-container">
+        <div className="QuestionID-head-container">
+          {question ? <div className="title-question">{question.title}</div> : null}
+          <Modal className="modal" isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+            <h1>Feeling Smart! 🤔</h1>
+            <h2>Well put that brain to good use and post an answer!</h2>
+            <ul className="answer_errors">
+              {errors.map(error => (
+                <li key={error}>{error}</li>
+              ))}
+            </ul>
+            <form onSubmit={onSubmit}>
+              <label htmlFor="comment">Comment</label>
+              <textarea rows="10" value={comment} onChange={e => setComment(e.target.value)} placeholder="Enter comment here..." required></textarea>
+              <button onClick={() => setModalIsOpen(false)}>close</button>
+              <button type="submit" disabled={errors.length > 0}>submit</button>
+            </form>
+          </Modal>
 
-        <button onClick={() => setModalIsOpen(true)}>Create Answer</button>
-        <Modal className="modal" isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-          <h1>Feeling Smart! 🤔</h1>
-          <h2>Well put that brain to good use and post an answer!</h2>
-          <ul className="answer_errors">
-            {errors.map(error => (
-              <li key={error}>{error}</li>
-            ))}
-          </ul>
-          <form onSubmit={onSubmit}>
-            <label htmlFor="comment">Comment</label>
-            <textarea rows="10" value={comment} onChange={e => setComment(e.target.value)} placeholder="Enter comment here..." required></textarea>
-            <button onClick={() => setModalIsOpen(false)}>close</button>
-            <button type="submit" disabled={errors.length > 0}>submit</button>
-          </form>
-        </Modal>
-
-      </div>
-      <div className="QuestionID-body-container">
-        {question ? <div className="title-question">{question.title}</div> : null}
-        {question ? <div className="comment-question">{question.comment}</div> : null}
-        {answersArr.map(answer => (
-          <div key={answer.id} className="post-answer">
-            <h3>Answer</h3>
-            <p className="post-answer_comment">{answer.comment}</p>
+        </div>
+        <div className="QuestionID-body-container">
+          {question ? <div className="comment-question">{question.comment}</div> : null}
+          {countAnswers !== 0 ? <div className="all_answers_text">{countAnswers} Answers</div> : <div className="no_answers_text">Be the first to answer!</div>}
+          {answersArr.map(answer => (
+            <div key={answer.id} className="post-answer">
+              <p className="post-answer_comment">{answer.comment}</p>
+            </div>
+          ))}
+          <div className="ask-question-container">
+            <button className="ask-question" onClick={() => setModalIsOpen(true)}>Post your answer</button>
           </div>
-        ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
